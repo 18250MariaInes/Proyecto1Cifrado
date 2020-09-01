3822d7704e12ffb26f47ab11b177f2773375102d
Maria Vasquez <maria.ines.vf@gmail.com>
2020-09-01 09:31:33 -0600

    remove DONE

--- pruebas/pruebas_funciones.py
+++ pruebas/pruebas_funciones.py
@@ -128,6 +128,40 @@ def get(name):
             conexion.close()
             print('Conexión finalizada.')
 
+def remove(name):
+    conexion = None
+    try:
+        # Lectura de los parámetros de conexion
+        params = config()
+
+        #print(params)
+        # Conexion al servidor de PostgreSQL
+        print('Conectando a la base de datos PostgreSQL...')
+        conexion = psycopg2.connect(**params)
+        # creación del cursor
+        cur = conexion.cursor()
+        # Ejecución la consulta para obtener la conexión
+        print('La version de PostgreSQL es la:')
+        cur.execute('SELECT version()')
+
+
+        # Se obtienen los resultados
+        db_version = cur.fetchone()
+        # Se muestra la versión por pantalla
+        print(db_version)
+        cur.execute("DELETE FROM passwords WHERE passwords.userid = %s AND passwords.site=%s",(2,name))
+        conexion.commit()      
+
+        
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conexion is not None:
+            conexion.close()
+            print('Conexión finalizada.')
+
+
 
 
 def conectar():
@@ -165,4 +199,5 @@ def conectar():
 
 if __name__ == '__main__':
     #set("facebook", "123456")
-    get("facebook")
+    #get("facebook")
+    #remove("facebook")
